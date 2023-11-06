26b719021e2c81a041e4578d271d5b2bc99438c9
Tiago Santi <tiagosanti2009@gmail.com>
2023-11-06 06:07:42 -0400

    add app main file

---
+++ myapp/app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_restx import Api
+from models import db
+from views.assinatura_view import assinatura_ns
+from views.configuracoes_notificacao_view import configuracoes_notificacao_ns
+from views.log_view import log_ns
+from views.pagamento_view import pagamento_ns
+from views.pedido_view import pedido_ns
+from views.pedido_produto_view import pedido_produto_ns
+from views.plano_view import plano_ns
+from views.produto_view import produto_ns
+from views.usuario_view import usuario_ns
+
+app = Flask(__name__)
+app.config.from_object('config.Config')
+db.init_app(app)
+
+api = Api(app,
+          version='1.0',
+          title='Facom Café API',
+          description='API para gerenciamento de assinaturas de café da Facom.')
+
+api.add_namespace(assinatura_ns)
+api.add_namespace(configuracoes_notificacao_ns)
+api.add_namespace(log_ns)
+api.add_namespace(pagamento_ns)
+api.add_namespace(pedido_ns)
+api.add_namespace(pedido_produto_ns)
+api.add_namespace(plano_ns)
+api.add_namespace(produto_ns)
+api.add_namespace(usuario_ns)
+
+if __name__ == '__main__':
+    app.run(debug=True)
