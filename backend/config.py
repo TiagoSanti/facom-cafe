0dabf93c30d53c9cc6931162e22f4369e718bbaf
Tiago Santi <tiagosanti2009@gmail.com>
2023-11-07 06:47:57 -0400

    altera a string de conexão para um banco local

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,3 @@
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://jzhvezia:GQv79DogAJUc9QIFctwtEcxOk3MrK-oq@isabelle.db.elephantsql.com/jzhvezia?options=-csearch_path%3Dfacom_cafe_schema'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/tads_local?options=-csearch_path%3Dfacom_cafe_schema'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
