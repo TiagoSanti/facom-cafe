f388cb0a504dfcee8e99c8781ad06529c6011261
Tiago Santi <tiagosanti2009@gmail.com>
2023-11-07 06:19:54 -0400

    adiciona as configuracoes do banco no controle de versao

---
+++ backend/config.py
@@ -0,0 +1,3 @@
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://jzhvezia:GQv79DogAJUc9QIFctwtEcxOk3MrK-oq@isabelle.db.elephantsql.com/jzhvezia?options=-csearch_path%3Dfacom_cafe_schema'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
