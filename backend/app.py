1dc1894591ae29ec13e0ff1943ad7a98ed69f5c6
Tiago Santi <tiagosanti2009@gmail.com>
2023-11-10 17:12:49 -0400

    [WIP] remove a migration e adiciona a população do banco

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_restx import Api
-from flask_migrate import Migrate
 from .extensions import db
+from .db_setup import create_and_populate_db
 from .views.assinatura_view import assinatura_ns
 from .views.configuracoes_notificacao_view import configuracoes_notificacao_ns
 from .views.log_view import log_ns
@@ -18,7 +18,9 @@ def create_app(DB_URI):
     app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 
     db.init_app(app)
-    migrate = Migrate(app, db)
+
+    with app.app_context():
+        create_and_populate_db()
 
     CORS(app)
     CORS(app, resources={r'/*': {'origins': '*'}})
