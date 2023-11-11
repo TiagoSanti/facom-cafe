4252f5d6a9f0ae40affe0f3b40a40411b1d343f4
Tiago Santi <tiagosanti2009@gmail.com>
2023-11-11 06:29:55 -0400

    ajusta o código para condizer com o readme

--- backend/run.py
+++ backend/run.py
@@ -5,8 +5,7 @@ import os
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-# LBD_DATABASE_URI e TADS_DATABASE_URI são variáveis de ambiente
-app = create_app(os.getenv('TADS_DATABASE_URI'))
+app = create_app(os.getenv('DATABASE_URI'))
 
 if __name__ == '__main__':
     app.run(debug=True)
